d687cafb16b22db7935025e3d17debe3c9e1be9d
Triin Kask <33719534+TriinK@users.noreply.github.com>
2017-12-17 17:39:39 +0200

    Replaced with a better code

--- Plot1.R.R
+++ Plot1.R.R
@@ -2,7 +2,10 @@
 
 data <- read.table('household_power_consumption.txt', sep = ';', check.names = TRUE, col.names = c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
 data = data[-1, ]
-data$Date <- as.Date( as.character(data$Date), "%d/%m/%y")
-hist(subset(data, Date == as.Date('2020-02-01'), select = as.numeric(data$Global_active_power)), main = "Global active power", xlab = "Global active power (kilowatts)", col = 'red')
-
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+result <- subset(data, Date == "2007/02/01" | Date == "2007/02/02")
+result$Global_active_power <- as.numeric(result$Global_active_power)
+hist(result$Global_active_power, main = "Global active power", xlab = "Global active power (kilowatts)", col = 'red')
+dev.copy(png, file="Plot1.png", width=480, height=480)
+dev.off()
 
